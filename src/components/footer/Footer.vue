e4ac88eacb5197e677225f8d7504e28b2bd9f34b
Mandy <812292994@qq.com>
2019-03-12 22:17:20 +0800

    Create Footer.vue

---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+   <div class="footer">
+     <p>Copyrigh© 2018-2019 Version</p>
+   </div>
+</template>
+
+<script lang="ts">
+   import {
+      Component,
+      Prop,
+      Vue
+   }
+   from 'vue-property-decorator';
+   @Component
+   export default class Footer extends Vue {}
+</script>
+
+<style scoped lang="scss">
+.footer{
+    background:$themecolor;
+    padding:10px;
+    @include text_center;
+    color:$color;
+}
+</style>
